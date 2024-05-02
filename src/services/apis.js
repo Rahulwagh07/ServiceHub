b0ff51d822e0cf4133f3c65e823f6d7e1eac7362
Rahulwagh07 <rahulwagh3774@gmail.com>
2024-05-02 12:39:08 +0530

    add vercel config

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:4000/api"
+const BASE_URL = import.meta.env.VITE_BASE_URL || "https://servicehub-backend.onrender.com/api"
 
 //Auth endpoints
 export const endpoints = {
